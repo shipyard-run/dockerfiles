77f39a129df682386dfece610ba6f9608161eef8
Erik Veld <eveld@hashicorp.com>
2021-10-07 07:57:01 +0200

    Add jdk to nomad image, fix data path, and bump version to 1.1.6

--- nomad/start_nomad.sh
+++ nomad/start_nomad.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 # Load the CA and add to the cache
 if [[ -z "${PROXY_CA}" ]]; then
   echo "Ignore custom certificates for the proxy"
 else
   echo "Adding cert ${PROXY_CA} to cache"
-  echo "${PROXY_CA}" > /usr/local/share/ca-certificates/proxy.crt
+  echo "${PROXY_CA}" > /etc/ssl/certs/proxy.crt
+  echo "${PROXY_CA}" >> /etc/ssl/certs/ca-certificates.crt
   update-ca-certificates
 fi
 
