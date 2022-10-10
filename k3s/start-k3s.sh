324d6139b8d9ddb03eb5c2ed8dd7f38d37f3bbb8
Nic Jackson <jackson.nic@gmail.com>
2022-10-10 10:23:42 +0100

    Update K3s and Nomad to latest

--- k3s/start-k3s.sh
+++ k3s/start-k3s.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [ -f /sys/fs/cgroup/cgroup.controllers ]; then
+	# move the processes from the root group to the /init group,
+  # otherwise writing subtree_control fails with EBUSY.
+  mkdir -p /sys/fs/cgroup/init
+  busybox xargs -rn1 < /sys/fs/cgroup/cgroup.procs > /sys/fs/cgroup/init/cgroup.procs || :
+  # enable controllers
+  sed -e 's/ / +/g' -e 's/^/+/' <"/sys/fs/cgroup/cgroup.controllers" >"/sys/fs/cgroup/cgroup.subtree_control"
+fi
 
 # Load the CA and add to the cache
 if [[ -z "${PROXY_CA}" ]]; then
@@ -7,7 +19,6 @@ else
   echo "Adding cert ${PROXY_CA} to cache"
   echo "${PROXY_CA}" > /etc/ssl/certs/proxy.crt
   echo "${PROXY_CA}" >> /etc/ssl/certs/ca-certificates.crt
-  update-ca-certificates
 fi
 
 /bin/k3s $@
